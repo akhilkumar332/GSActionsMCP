ff11b92fb3b2c889361244f4f23721880e77c730
akhilkumar332 <akhilkumar332@gmail.com>
2026-05-14 08:43:42 +0200

    test: prepare analytics test file

---
+++ cmd/server/api_analytics_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandleGetTrends(t *testing.T) {
+	// Setup will be added in the RED step
+}
